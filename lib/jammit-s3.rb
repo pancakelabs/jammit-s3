f9afd6ffe86f747b530ea08d3977e9690303b664
Ben M <ben@shortstacklab.com>
2013-07-10 11:26:25 -0700

    Removed SimpleConfig stuff. Rewrote Cloudfront support.

--- lib/jammit-s3.rb
+++ lib/jammit-s3.rb
@@ -6,39 +6,28 @@ module Jammit
   def self.upload_to_s3!(options = {})
     S3Uploader.new(options).upload
   end
-end
 
-if defined?(Rails)
-  module Jammit
-    class JammitRailtie < Rails::Railtie
-      initializer "set asset host and asset id" do
-        config.before_initialize do
-          if Jammit.configuration[:use_cloudfront] && Jammit.configuration[:cloudfront_cname].present? && Jammit.configuration[:cloudfront_domain].present?
-            asset_hostname = Jammit.configuration[:cloudfront_cname]
-            asset_hostname_ssl = Jammit.configuration[:cloudfront_domain]
-          elsif Jammit.configuration[:use_cloudfront] && Jammit.configuration[:cloudfront_domain].present?
-            asset_hostname = asset_hostname_ssl = Jammit.configuration[:cloudfront_domain]            
-          else
-            asset_hostname = asset_hostname_ssl = "#{Jammit.configuration[:s3_bucket]}.s3.amazonaws.com"
-          end
+  # Generates the server-absolute URL to an asset package.
+  def self.asset_url(package, extension, suffix=nil, mtime=nil)
+    if Jammit.configuration[:use_cloudfront] && Jammit.configuration[:cloudfront_cname].present? && Jammit.configuration[:cloudfront_domain].present?
+      asset_hostname = Jammit.configuration[:cloudfront_cname]
+      asset_hostname_ssl = Jammit.configuration[:cloudfront_domain]
+    elsif Jammit.configuration[:use_cloudfront] && Jammit.configuration[:cloudfront_domain].present?
+      asset_hostname = asset_hostname_ssl = Jammit.configuration[:cloudfront_domain]            
+    else
+      asset_hostname = asset_hostname_ssl = "#{Jammit.configuration[:s3_bucket]}.s3.amazonaws.com"
+    end
+      
+    protocol = (Jammit.configuration.has_key?(:ssl) && Jammit.configuration[:ssl]) ? "https://" : "http://"
 
-          if Jammit.package_assets and asset_hostname.present?
-            puts "Initializing Cloudfront"                      
-            ActionController::Base.asset_host = Proc.new do |source, request|
-              if Jammit.configuration.has_key?(:ssl)
-                protocol = Jammit.configuration[:ssl] ? "https://" : "http://"
-              else
-                protocol = request.protocol
-              end
-              if request.protocol == "https://"
-                "#{protocol}#{asset_hostname_ssl}"
-              else 
-                "#{protocol}#{asset_hostname}"  
-              end              
-            end
-          end
-        end
-      end
+    asset = Jammit.configuration[:s3_upload_files].find{|asset| asset["src"].gsub(/.*\//, '') == "#{package}.#{extension}"}
+    asset_path = asset["dest"]
+
+    timestamp = mtime ? "?#{mtime.to_i}" : ''
+    if protocol == "https://"
+      "#{protocol}#{asset_hostname_ssl}/#{asset_path}#{timestamp}"
+    else 
+      "#{protocol}#{asset_hostname}/#{asset_path}#{timestamp}"
     end
   end
 end
